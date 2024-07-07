5570e7e4c1809363e70b36838a7de8c1e5a205ef
TonyR <tonyrodriguez26@outlook.com>
2024-07-07 01:47:32 -0400

    added new css and js files for the gallery page

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // script.js
-
 document.addEventListener('DOMContentLoaded', () => {
-    const slides = document.querySelectorAll('.my-slide');
-    let currentIndex = 0;
-    
-    function showNextSlide() {
-      slides[currentIndex].classList.remove('my-active');
-      currentIndex = (currentIndex + 1) % slides.length;
-      slides[currentIndex].classList.add('my-active');
-    }
-    
-    setInterval(showNextSlide, 3000); // Change slide every 3 seconds
-  });
+  const slides = document.querySelectorAll('.my-slide');
+  let currentIndex = 0;
 
+  function showNextSlide() {
+    slides[currentIndex].classList.remove('my-active');
+    currentIndex = (currentIndex + 1) % slides.length;
+    slides[currentIndex].classList.add('my-active');
+  }
+
+  setInterval(showNextSlide, 3000); // Change slide every 3 seconds
+
+});
+
+
+$("#landing-page").fadeIn(2000);
