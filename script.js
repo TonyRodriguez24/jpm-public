e4b6e044dec1424766dc90ec70f5756d4178f326
TonyR <tonyrodriguez26@outlook.com>
2024-07-16 17:11:48 -0400

    added

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ $(document).ready(function() {
   $("#landing-page").css("visibility", "visible").animate({opacity: 1}, 2000);
 });
 
+
+$("#contact-form").on("submit", function(e){
+  // Check if the form is valid
+  if (this.checkValidity()) {
+    e.preventDefault();
+    $("#contact-form").hide();  
+    $("#form-success-container").show();
+    $(".form-container h2").text("JPM and Sons");
+  } else {
+    // If the form is not valid, let the browser handle the validation
+    // This will show the validation messages for required fields
+    $(this).find(":submit").click(); 
+  }
+});
