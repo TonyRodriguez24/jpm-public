da50e6315686ee4dbb32ea62609b557895a726a8
TonyR <tonyrodriguez26@outlook.com>
2024-07-18 17:08:41 -0400

    updated gallery animation so that ones in view load nicely as well

--- src/gallery.js
+++ src/gallery.js
@@ -1,18 +1,23 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    const observerOptions = {
-      threshold: 0.1
-    };
+document.addEventListener('DOMContentLoaded', () => {
+  const observerOptions = {
+    threshold: 0.1
+  };
 
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('fade-in');
-          observer.unobserve(entry.target);
-        }
-      });
-    }, observerOptions);
-
-    document.querySelectorAll('#entire-gallery img').forEach(img => {
-      observer.observe(img);
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in');
+        observer.unobserve(entry.target);
+      }
     });
+  }, observerOptions);
+
+  document.querySelectorAll('#entire-gallery img').forEach(img => {
+    // Check if the image is already in view
+    if (img.getBoundingClientRect().top < window.innerHeight) {
+      img.classList.add('fade-in');
+    } else {
+      observer.observe(img);
+    }
   });
+});
