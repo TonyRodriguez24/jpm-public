7b87510e60bfd504864acbfc04e39954c38f7208
TonyR <tonyrodriguez26@outlook.com>
2024-07-21 18:35:59 -0400

    fixed css

--- src/slideshow.js
+++ src/slideshow.js
@@ -1,15 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const slides = document.querySelectorAll('.my-slide');
-    let currentIndex = 0;
-  
-    function showNextSlide() {
-      slides[currentIndex].classList.remove('my-active');
-      currentIndex = (currentIndex + 1) % slides.length;
-      slides[currentIndex].classList.add('my-active');
-    }
-  
-    if (slides.length > 0) {
-      setInterval(showNextSlide, 3000); // Change slide every 3 seconds
-    } 
-  });
-  
+  const slides = document.querySelectorAll('.why-us-img');
+  let currentIndex = 0;
+
+  function showNextSlide() {
+    slides[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % slides.length;
+    slides[currentIndex].classList.add('active');
+  }
+
+  if (slides.length > 0) {
+    setInterval(showNextSlide, 3000); // Change slide every 3 seconds
+  }
+});
+
+$(document).ready(function () {
+  let currentIndex = 0;
+  const images = $('.why-us-img');
+  const imageCount = images.length;
+
+  setInterval(function () {
+    const nextIndex = (currentIndex + 1) % imageCount;
+    $(images[currentIndex]).removeClass('active');
+    $(images[nextIndex]).addClass('active');
+    currentIndex = nextIndex;
+  }, 6000); // Change image every 3 seconds
+});
