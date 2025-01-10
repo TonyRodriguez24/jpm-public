5ae05d1fbf995d15def44a9058b5e6554d076140
TonyRodriguez26 <tonyrodriguez26@outlook.com>
2025-01-09 21:40:30 -0500

    updated app, config, database

--- app/app.py
+++ app/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from database import db, connect_db
+import os
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///playlist_app')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['WTF_CSRF_ENABLED'] = True  # Ensure CSRF is enabled
+app.config['SECRET_KEY'] = 'asdfasdf'
+
+connect_db(app)
 
 @app.route('/')
 def home():
